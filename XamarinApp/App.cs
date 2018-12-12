abc76d01bd1c9026b42b7bd41fd6c42b6300038d
Roger Weiss <roger@cowboyaliens.com>
2018-12-12 17:22:15 +0200

    Initial commit

---
+++ XamarinApp/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace XamarinApp
+{
+    public class App
+    {
+        public static string BackendUrl = "http://localhost:5000";
+
+        public static void Initialize()
+        {
+            ServiceLocator.Instance.Register<IDataStore<Item>, MockDataStore>();
+        }
+    }
+}
